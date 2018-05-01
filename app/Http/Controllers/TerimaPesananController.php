369a1a079240a0a7d82985d222d6bd695cb85a48
Cross\symbiancross <atomicsecure@gmail.com>
2018-05-02 03:46:53 +0700

    menerima pesanan

--- app/Http/Controllers/TerimaPesananController.php
+++ app/Http/Controllers/TerimaPesananController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Auth;
 use App\Tukang;
 use App\Pesan;
+use App\User;
+use App\Pekerjaan;
 
 class TerimaPesananController extends Controller
 {
@@ -18,17 +22,89 @@ class TerimaPesananController extends Controller
     {
         $pesans = Pesan::with('user')->get();
 
-        return view('pesantukang.terima-pesanan')->with('pesans', $pesans);
+        $pesanan = DB::table('pesans')->where('tukang_id', '=', Auth::user()->tukang_id)->whereIn('isComplete', [1, 2])->get();
+
+        $cek_pesanan = 0;
+        $detail_user = new User;
+        $pesanan_id = 0;
+        
+        if(count($pesanan)==1)
+        {
+            $cek_pesanan = 1;
+            $detail_user = User::findOrFail($pesanan[0]->user_id);
+        }
+
+        return view('pesantukang.terima-pesanan')->with('pesans', $pesans)->with('cek_pesanan', $cek_pesanan)->with('detail_user', $detail_user)->with('pesanan', $pesanan);
     }
 
-    public function terimaPesanan(Request $request, $id)
+    public function status($id)
     {
         $pesan = Pesan::findOrFail($id);
+        if($pesan->isComplete==0)
+        {
+            $pesan->isComplete = 1;
+            $pesan->save();
+            return redirect()->route('daftar.pesanan');
+        }
+        else if($pesan->isComplete==1)
+        {
+            $pesan->isComplete = 2;
+            $pesan->save();
+            return redirect()->route('daftar.pesanan');
+        }
+        else
+        {
+            $pesan->isComplete = 3;
+            $pesan->save();
+            return redirect('tukang');
+        }   
+    }
 
-        $pesan->tukang_id = Auth::user()->tukang_id;
-        $pesan->isComplete = 1;
+    public function showTambahPembayaran()
+    {
+        $pesan = DB::table('pesans')->where('tukang_id', '=', Auth::user()->tukang_id)->where('isComplete', '=', 2)->get();
+        $kekurangans = new Pekerjaan;
+        $cek_pesanan = 0;
+        if(count($pesan)==1)
+        {
+            $cek_pesanan = 1;            
+            $pesan_id = $pesan[0]->pesan_id;
+            $kekurangans = DB::table('pekerjaans')->where('tukang_id', '=', Auth::user()->tukang_id)->where('pesan_id', '=', $pesan_id)->get();
+            return view('pesantukang.tambah-biaya')->with('kekurangans', $kekurangans)->with('cek_pesanan', $cek_pesanan);
+        }
+        else
+        {
+            return view('pesantukang.tambah-biaya')->with('kekurangans', $kekurangans)->with('cek_pesanan', $cek_pesanan);
+        }
+        
+    }
 
-        $pesan->save();
-        return redirect('tukang');
+    public function tambahKekurangan(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'kekurangan' => 'required|max:255',
+            'harga' => 'required|numeric|between:0.001,999999999',
+        ]);
+
+        $pesan = DB::table('pesans')->where('tukang_id', '=', Auth::user()->tukang_id)->where('isComplete', '=', 2)->get();
+        $pesan_id = $pesan[0]->pesan_id;
+
+        $pekerjaan =  new Pekerjaan;
+        $pekerjaan->tukang_id = Auth::user()->tukang_id;
+        $pekerjaan->pekerjaan = $request->kekurangan;
+        $pekerjaan->harga = $request->harga;
+        $pekerjaan->pesan_id = $pesan_id;
+
+        $pekerjaan->save();
+
+        return redirect()->route('biaya');
+    }
+
+    public function hapusKekurangan($id)
+    {
+        $pekerjaan = Pekerjaan::findOrFail($id);
+        $pekerjaan->delete();
+
+        return redirect()->route('biaya');
     }
 }
--- resources/views/pesantukang/terima-pesanan.blade.php
+++ resources/views/pesantukang/terima-pesanan.blade.php
@@ -2,42 +2,66 @@
 
 @section('content')
 @if (count($pesans) > 0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Current Tasks
-                    </div>
+    @if($cek_pesanan == 1)
+        @if ($pesanan[0]->isComplete == 1 || $pesanan[0]->isComplete == 2)
+            <center>
+                {{ $detail_user->nama }}<br>
+                {{ $detail_user->alamat }}<br>
+                {{ $detail_user->no_telp }}
+                @if ($pesanan[0]->isComplete == 1)
+                    <form action="{{ route('status', $pesanan[0]->pesan_id) }}" method="POST">
+                            {{ csrf_field() }}
 
-                    <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <thead>
-                                <th>Pesanan</th>
-                                <th>&nbsp;</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($pesans as $pesan)
-                                @if ( $pesan->keahlian == Auth::guard('tukang')->user()->keahlian_id && $pesan->isComplete == 0)
-                                    <tr>
-                                        <td class="table-text"><div>{{ $pesan->user->nama }}</div></td>
-                                        <td class="table-text"><div>{{ $pesan->keahlian }}</div></td>
+                        <button type="submit" class="btn btn-success">
+                            <i class="fa fa-btn fa-trash"></i>Sampai
+                        </button>
+                    </form>
+                @elseif ($pesanan[0]->isComplete == 2)
+                    <form action="{{ route('biaya') }}" method="GET">
 
-                                        <!-- Task Delete Button -->
-                                        <td>
-                                            <form action="{{ route('terima.pesanan', $pesan->pesan_id) }}" method="POST">
-                                                {{ csrf_field() }}
-
-                                                <button type="submit" class="btn btn-success">
-                                                    <i class="fa fa-btn fa-trash"></i>Terima
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endif
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            @endif
+                        <button type="submit" class="btn btn-success">
+                            <i class="fa fa-btn fa-trash"></i>Total
+                        </button>
+                    </form>
+                @endif
+            </center>
+        @endif
+    @else
+        <div class="panel panel-default">
+        <div class="panel-heading">
+            Current Tasks
         </div>
-</div>
+
+        <div class="panel-body">
+            <table class="table table-striped task-table">
+                <thead>
+                    <th>Pesanan</th>
+                    <th>&nbsp;</th>
+                </thead>
+                <tbody>
+                    @foreach ($pesans as $pesan)
+                        @if ($pesan->keahlian == Auth::guard('tukang')->user()->keahlian_id && $pesan->isComplete == 0)
+                            <tr>
+                                <td class="table-text"><div>{{ $pesan->user->nama }}</div></td>
+                                <td class="table-text"><div>{{ $pesan->keahlian }}</div></td>
+
+                                <!-- Task Delete Button -->
+                                <td>
+                                    <form action="{{ route('status', $pesan->pesan_id) }}" method="POST">
+                                        {{ csrf_field() }}
+
+                                        <button type="submit" class="btn btn-success">
+                                            <i class="fa fa-btn fa-trash"></i>Terima
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endif
+                    @endforeach            
+                </tbody>
+            </table>
+        </div>
+        </div>
+    @endif
+@endif
 @endsection
